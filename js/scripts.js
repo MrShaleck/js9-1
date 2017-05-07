b9862355016ece6e2a89158465c4d6cd1fceebb7
Marcin <marcinrszalek@gmail.com>
2017-05-07 14:43:15 +0200

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,19 @@
-	function getTriangleArea(a, h) {
+function getTriangleArea(a, h) {
 	
 	if ( (a > 0) && (h > 0) ) {
-		return a*h/2;
+		return a * h / 2;
 	}
-	else if ( (a <= 0) || (h <= 0) ) {
-	console.log('Nieprawid³owe dane!');
-	};
+		else {
+			console.log('NieprawidÅ‚owe dane!')
+	}
 
-	
-
-};
+}
 
 var triangle1Area = getTriangleArea(7, 7);
 var triangle2Area = getTriangleArea(9, 5);
 var triangle3Area = getTriangleArea(11, 16);
 
-console.log( getTriangleArea(10,6) );
-console.log( triangle1Area);
-console.log( triangle2Area);
-console.log( triangle3Area);
+console.log(getTriangleArea(10,6) );
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
